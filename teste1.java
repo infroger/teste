8e5d6bb83345cea3abb97fb50c67d29c3e950001
Roger Al-Alam Krolow <roger.krolow@uniritter.local>
2019-05-14 21:42:54 -0300

    Adição da terceira mensagem

--- teste1.java
+++ teste1.java
@@ -3,6 +3,7 @@ class Teste1 {
 	public static void main (String[] args) {
 		system.out.println("OLá Mundo!");
 		system.out.println("Segunda mensagem!");
+		system.out.println("Terceira mensagem!");
 	}
 
 }
